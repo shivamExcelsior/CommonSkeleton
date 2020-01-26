03fd45e687b103bee8706b6a4b4b70481a85ffd0
shivamExcelsior <shivams054@gmail.com>
2020-01-26 13:43:29 +0530

    basic register component frontend only with validations, password match pending

--- commonSkeletonUI/src/app/components/register/register.component.ts
+++ commonSkeletonUI/src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,78 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup
+  genders: string[] = ['Male', 'Femail', 'Other']
+  hide = true
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.buildRegisterForm()
+  }
+
+  buildRegisterForm() {
+    this.registerForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      dob: ['', Validators.required],
+      selectGender: ['Male', Validators.required],
+      password: ['', Validators.required],
+      cpassword: ['', Validators.required]
+    }, { Validator: this.checkIfMatchingPasswords('password', 'cpassword') })
+  }
+
+  checkIfMatchingPasswords(passwordKey: string, passwordConfirmationKey: string) {
+    return (group: FormGroup) => {
+      let passwordInput = group.controls[passwordKey],
+          passwordConfirmationInput = group.controls[passwordConfirmationKey];
+      if (passwordInput.value !== passwordConfirmationInput.value) {
+        return passwordConfirmationInput.setErrors({notEquivalent: true})
+      }
+      else {
+          return passwordConfirmationInput.setErrors(null);
+      }
+    }
+  }
+
+  get name() {
+    return this.registerForm.get('name')
+  }
+
+  get username() {
+    return this.registerForm.get('username')
+  }
+
+  get email() {
+    return this.registerForm.get('email')
+  }
+
+  get dob() {
+    return this.registerForm.get('dob')
+  }
+
+  get selectGender() {
+    return this.registerForm.get('selectGender')
+  }
+
+  get password() {
+    return this.registerForm.get('password')
+  }
+
+  get cpassword() {
+    return this.registerForm.get('cpassword')
+  }
+
+  onRegisterSubmit(formId: NgForm) {
+    if(this.registerForm.invalid) {
+      return false
+    } else {
+      console.log('Form Data:', this.registerForm.value)
+      formId.resetForm()
+      this.buildRegisterForm()
+    }
   }
 
 }
